559decd0542ad2d45be17fea48b300b7da813026
Neon <fatehzaliyev@gmail.com>
2023-05-25 11:20:16 +0400

    Categorization init

--- src/main/java/com/ktxdevelopment/bailyapi/security/config/ApplicationConfig.java
+++ src/main/java/com/ktxdevelopment/bailyapi/security/config/ApplicationConfig.java
@@ -21,7 +21,7 @@ public class ApplicationConfig {
 
     @Bean
     public UserDetailsService userDetailsService() {
-        return username -> repository.findByEmail(username);
+        return repository::findByEmail;
     }
 
     @Bean
